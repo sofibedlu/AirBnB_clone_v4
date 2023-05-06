b019a371b70e7aefa85d0c610e963c34d4d80099
sofi <bedlusofi@gmail.com>
2023-05-06 07:09:37 +0000

    task-3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  const amenitiesChecked = {};
+  $('input[type="checkbox"]').on('change', function () {
+    const isChecked = $(this).is(':checked');
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if (isChecked) {
+      amenitiesChecked[amenityId] = amenityName;
+    } else {
+      delete amenitiesChecked[amenityId];
+    }
+
+    const checkedAmenities = Object.values(amenitiesChecked);
+    const amenitiesList = checkedAmenities.join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
